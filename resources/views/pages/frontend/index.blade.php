68275e61593431b88be8cff61ec59300ca9ac9ab
WepssPros <reyhandwiambrta@gmail.com>
2024-05-24 21:44:11 +0700

    Skripsi Pak Raharjo Phase 1

--- resources/views/pages/frontend/index.blade.php
+++ resources/views/pages/frontend/index.blade.php
@@ -540,16 +540,16 @@ loading="lazy" />
 <section class="cta" id="contact">
     <div class="container">
         <div class="cta-content">
-            <p class="section-subtitle">Call To Action</p>
+            <p class="section-subtitle">Universitas Nurdin Hamzah</p>
 
             <h2 class="h2 section-title">
-                Ready For Unforgatable Travel. Remember Us!
+                Sistem Informasi Pengawasan
             </h2>
 
             <p class="section-text">
-                Fusce hic augue velit wisi quibusdam pariatur, iusto primis, nec
-                nemo, rutrum. Vestibulum cumque laudantium. Sit ornare mollitia
-                tenetur, aptent.
+                MASA BERLAKU KENDARAAN LAIK JALAN SECARA ONLINE DI UPTD PENGUJIAN
+                KENDARAAN BERMOTOR DINAS PERHUBUNGAN KABUPATEN TANJUNG JABUNG
+                BARAT
             </p>
         </div>
 
